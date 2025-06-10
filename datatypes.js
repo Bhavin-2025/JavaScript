1ca503a1cdcbc90fae680a169bd2f606e383d069
Bhavin Giniya <bhavin@sarvadhi.com>
2025-06-10 15:44:31 +0530

    datatypes.js

---
+++ datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; 
+// treat all js code as newer version
+
+console.log(3+3);
+
+
+let name = "Bhavin";
+
+let age = 21;
+
+let isLoggedIn = false;
+
+
+// number => 2 to power53
+// bigint
+// string => ""
+// boolean => true/false
+//  null => standalone value
+//  undefined =>
+//  symbol => unique
+// object
+
+console.log(typeof "Bhavin");
+console.log(typeof 21);
+console.log(typeof null);
+console.log(typeof undefined);
