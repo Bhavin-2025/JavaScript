c8e37eda26226a5464d7163876faa84f22ca70dc
Bhavin Giniya <bhavin@sarvadhi.com>
2025-06-10 15:15:11 +0530

    Initial commit

---
+++ variable.js
@@ -0,0 +1,35 @@
+var Username = "Bhavin";
+// console.log("Name: "+ Username);
+
+
+let message;
+message = 'Hello!';
+// console.log(message);
+
+
+// A repeated declaration of the same variable is an error:
+
+// let firstName = "Bhavin";
+// let firstName ="abc"
+
+
+
+let $ = 1; // declared a variable with the name "$"
+let _ = 2; // and now a variable with the name "_"
+// console.log($ + _); // 3
+
+
+const myBirthday = '18.04.1982';
+// myBirthday = '01.01.2001'; // error, can't reassign the constant!
+// console.log(myBirthday);
+
+
+let admin;
+let admin_name;
+admin_name = "john";
+admin = admin_name;
+console.log(admin);
+
+let ourPlanetName ="Earth";
+
+let currentUserName="John";
