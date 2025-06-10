160b20dc95a1d17796b36dec8b72f48cb8556022
Bhavin Giniya <bhavin@sarvadhi.com>
2025-06-10 17:32:48 +0530

    updated file

--- conversion.js
+++ conversion.js
@@ -37,3 +37,42 @@ let someNumber = 33
 let stringNumber= String(someNumber)
 // console.log(typeof stringNumber); string
 // console.log(stringNumber) 33
+
+
+// ***************operations*********************************
+
+
+let value = 3
+let negValue = -value
+// console.log(negValue); -3
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%2);
+
+
+let str1 = "hello"
+let str2 = " bhavin"
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+// 
+
+
+
+let x = 16 + 4 + "Volvo";
+// console.log(x); 20Volvo
+
+let y = "Volvo" + 16 + 4;
+// console.log(y); Volvo164
+
+
+
+const a = Symbol('id');
+const b = Symbol('id');
+
+// console.log(a === b); // false
