1b0b8657dae20bdd49310c6f84a90b8bfe8c1305
Bhavin Giniya <bhavin@sarvadhi.com>
2025-06-11 10:56:35 +0530

    primitive.js

---
+++ object.js
@@ -0,0 +1,14 @@
+const person={};
+
+person.firstName ="Bhavin";
+person.age=21;
+
+// const person = new Object();
+
+// object literals
+const JsUser ={
+    name:"Bhavin",
+    age: 21,
+    location:"Surat"
+}
+J
