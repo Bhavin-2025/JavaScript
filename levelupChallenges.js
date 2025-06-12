6c49f9aa509691736f7693eb5bbdeef47e4a5100
Bhavin Giniya <bhavin@sarvadhi.com>
2025-06-12 11:11:21 +0530

    updated file

--- levelupChallenges.js
+++ levelupChallenges.js
@@ -33,5 +33,120 @@ for (const num of numbers) {
     }
     smallNumbers.push(num)
 }
-console.log(smallNumbers);
+// console.log(smallNumbers);
 
+
+// Challenge 4 Goal is same as 2nd challenge but need to use For each loop
+
+let teaTypes =['green tea','black tea','herbal tea','chai','oolong tea']
+let prefered_tea =[]
+
+for (const tea of teaTypes) {
+    if(tea==='herbal tea'){
+        continue
+    }
+    prefered_tea.push(tea)
+}
+// console.log(new_tea);
+
+
+// Challenge 5
+// Use for in loop to loop through object containing city population
+// stop the loop  when the population of berlin is found and store all
+// all previous cities in a new object named 'cityPopulations'.
+
+let citiesPopulation ={
+    London: 100,
+    "New York": 200,
+    Syndey: 300,
+    Berlin: 400,
+    LasVegas: 200,
+   
+};
+
+let citiesPopulations ={}
+// console.log(Object.keys(citiesPopulation)); [ 'London', 'New York', 'Syndey', 'Berlin', 'Las Vegas' ]
+// console.log(Object.values(citiesPopulation)); [ 100, 200, 300, 400, 200 ]
+
+for (const  city in citiesPopulation) {
+    // key = value
+
+    if(city=="Berlin"){
+        break;
+    }
+    citiesPopulations[city]= citiesPopulation[city]
+    // console.log(citiesPopulations)
+
+        // console.log(citiesPopulation[city]); 100 200 300 400 200 but all in separate line output
+        
+    }
+
+
+    // Challenge 6
+
+    let worldCities ={
+    Syndey: 300,
+    Berlin: 400,
+    LasVegas: 200,
+};
+    let largeCities ={}
+    for(const city in worldCities){
+        if(worldCities[city]<250){
+            continue
+        }
+        largeCities[city]= worldCities[city]
+    }
+
+    // console.log(largeCities);
+
+    // challenge 7
+     
+    let teaCollection = ["earl grey","green tea","chai","oolong tea"]
+    let availableTeas=[]
+
+    teaCollection.forEach(function(tea) {
+  if(tea==='chai'){
+    return;
+  }  
+  availableTeas.push(tea);
+    });
+    // console.log(availableTeas);[ 'earl grey', 'green tea', 'oolong tea' ]
+    
+ //  challenge 8
+ let myWorldCities = ["Berlin","Tokyo","Sydney","Paris"]
+
+ let traveledCities =[];
+ myWorldCities.forEach(city => {
+    if(city==="Sydney"){
+        return;
+    }
+    traveledCities.push(city);
+ });
+//  console.log(traveledCities); [ 'Berlin', 'Tokyo', 'Paris' ]
+ 
+
+// Challenge 9
+
+let myNumbers =[2,5,7,9]
+let doubleNumbers=[]
+
+for (let i = 0; i < myNumbers.length; i++) {
+    if(myNumbers[i]===7){
+        continue;
+    }
+    doubleNumbers.push(myNumbers[i]*2); 
+    
+}
+// console.log(doubleNumbers); [ 4, 10, 18 ]
+
+
+let totalTea = ["chai","black tea","greenish tea","oolong tea","herbal tea"]
+let shorTea=[]
+
+for (const tea of totalTea) {
+    if(tea.length>10){
+        continue
+    }
+    shorTea.push(tea)
+}
+console.log(shorTea)
